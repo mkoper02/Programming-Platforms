2dcad1528f6c2d4fb8c12e36c1c73ed3eb999155
mkoper02 <maciej.koperdowski@gmail.com>
2024-03-03 23:26:10 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿namespace FizzBuzz {
+    internal class FizzBuzz {
+        private int _limit;
+
+        public FizzBuzz(int _limit) {
+            this._limit = _limit;
+        }
+
+        public void display() {
+            for (int i = 1; i <= _limit; i++) {
+                if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0) Console.WriteLine("Fizz");
+                else if (i % 5 == 0) Console.WriteLine("Buzz");
+                else Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzz {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            FizzBuzz obj = new FizzBuzz(n);
+            obj.display();
+        }
+    }
+}
